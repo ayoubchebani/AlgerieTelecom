88da0c18762660ed41d26c9b36885bc75c6e65c1
Chebani Ahmed Ayoub <40341212+ayoubchebani@users.noreply.github.com>
2019-07-09 09:14:10 +0000

    Add files via upload

--- views.py
+++ views.py
@@ -13,4 +13,28 @@ def help(request):
     dict = {'here':problem}
     return render(request,'help.html',context=dict)
 
-# Create your views here.
+def button1(request):
+    return render(request,'index.html')
+
+def funct1(request):
+    print("unconfined")
+    data = "ayoub"
+    clicked = True
+    
+    return render(request,'index.html',context={'data':data})
+
+def button2(request):
+    data = "ayoub"
+    return render(request,'index.html',context={'data':data})
+
+def funct2(request):
+    mandatory = "mandatory"
+    print("ayoub mandatory")
+    dictionnary = {'data':mandatory}
+    
+    return render(request,'index.html',context=dictionnary)
+
+def menu(request):
+    return render(request, 'menu.html')
+
+
